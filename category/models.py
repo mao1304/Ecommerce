a404ebe81fabaf00a12a869fbaeb7fe7a291f670
mao1304 <eduarmoreno626@gmail.com>
2024-03-10 16:52:43 -0500

    accounts y category creados sin endpoints

---
+++ category/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    Category_name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    slug = models.CharField(max_length=100, unique=True)
+    cat_image = models.ImageField(upload_to='images/categories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+    
+    def __str__(self):
+        return self.Category_name

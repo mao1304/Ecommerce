c91158d296dbbfe16c5590e5e2dc79517173d610
mao1304 <eduarmoreno626@gmail.com>
2024-04-23 07:25:37 -0500

    antes de  tratar de arreglar el error de pinzon

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     Product_name = models.CharField(max_length=200, unique=True)
     slug = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=500, blank=True)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=10, decimal_places=3)
     brand = models.CharField(max_length=100)
     images = models.CharField( max_length=250)
     stock = models.IntegerField()
@@ -17,7 +17,9 @@ class Product(models.Model):
     created_date = models.DateTimeField( auto_now_add=True)
     modified_date = models.DateTimeField( auto_now=True)
 
-    
+    def formatted_price(self):
+        return '{:,.0f}'.format(self.price) 
+
     def get_url(self):
         return reverse('product_detail', args=[self.SubCategory.slug, self.slug])
 
